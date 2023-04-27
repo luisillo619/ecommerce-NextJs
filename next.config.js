a2c086cd3a0852bab44b2392438da355d32c975b
luisillo619 <luiscarlosrangel619@gmail.com>
2023-04-27 12:16:46 -0600

    add .env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    DB_URI: process.env.DB_URI,
+    API_URL: process.env.API_URL,
+    CLIENT_URL: process.env.CLIENT_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    CLOUD_NAME: process.env.CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  },
   images: {
     domains: ["res.cloudinary.com"],
   },

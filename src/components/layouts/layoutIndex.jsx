64ad4e9fa1cb963cf5b9d093578c78ed8ac9a3e2
luisillo619 <luiscarlosrangel619@gmail.com>
2023-05-04 14:16:07 -0600

    FEAT: stripe webhook controller

--- src/components/layouts/layoutIndex.jsx
+++ src/components/layouts/layoutIndex.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "./Header";
 import { setCart } from "@/redux/reducer/cartSlice";
-import { useDispatch } from "react-redux";
 import { setUser } from "@/redux/reducer/authSlice";
+import { selectCart } from "@/redux/reducer/cartSlice";
+import { selectUser } from "@/redux/reducer/authSlice";
 import { useSession } from "next-auth/react";
 
 export default function LayoutIndex({ children }) {
-  // Añadir estado inicial del carrito y de la sesión del usuario a redux
   const dispatch = useDispatch();
-  const { data } = useSession(); // Obtener la sesión del usuario si existe
-  const [isLoaded, setIsLoaded] = useState(false);
+  const { data, status } = useSession();
+  const cart = useSelector(selectCart);
+  const user = useSelector(selectUser);
+  const isLoaded = status === "authenticated" || status === "unauthenticated";
 
   useEffect(() => {
-    dispatch(setCart());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (data) {
-      dispatch(setUser(data.user));
+    if (isLoaded) {
+      dispatch(setCart());
+      if (data) {
+        dispatch(setUser(data.user));
+      }
     }
-    setIsLoaded(true);
-  }, [data, dispatch]);
+  }, [data, dispatch, isLoaded]);
 
-  if (!isLoaded) {
+  if (!user) {
     return null;
   }
 
   return (
     <>
-      <Header />
+      <Header user={user} cart={cart}/>
       {children}
     </>
   );

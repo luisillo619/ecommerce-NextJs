1e5c09e25af48617ed78c6215c721cb29cb86de9
luisillo619 <luiscarlosrangel619@gmail.com>
2023-05-05 22:30:37 -0600

    Improve user commands, enhance responsiveness, and fix loading state bugs

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -66,9 +66,8 @@ const Login = () => {
         <button
           type="submit"
           className="my-2 px-4 py-2 text-center w-full inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700"
-      
         >
-         Ingresar
+          Ingresar
         </button>
 
         <hr className="mt-4" />

b62f919d850ae7aa4c85213d0cf85052f85ff4c2
luisillo619 <luiscarlosrangel619@gmail.com>
2023-05-06 10:33:45 -0600

    FIX:redirect-errors-in-login

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,7 +1,24 @@
 import Register from "@/components/auth/Register";
+import { parseCallbackUrl } from "@/helpers/helpers";
+import { getSession } from "next-auth/react";
 
-
-// si existe session y se esta loageado entonces redirijir a "/"
+export const getServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+  if (session) {
+  
+    return {
+      redirect: {
+        destination: context.query.callbackUrl
+          ? parseCallbackUrl(context.query.callbackUrl)
+          : "/",
+        permanent: true,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+};
 
 const RegisterPage = () => {
   return <Register />;

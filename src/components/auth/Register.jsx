c7254023136af554fbc5a62b735f838aa557a309
luisillo619 <luiscarlosrangel619@gmail.com>
2023-05-07 23:50:12 -0600

    FIX: fix login and register styles

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -6,6 +6,7 @@ import { toast, Zoom } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 import { clearError } from "@/redux/reducer/authSlice";
 import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
 
 const Register = () => {
   const dispatch = useDispatch();
@@ -34,69 +35,82 @@ const Register = () => {
   };
 
   return (
-    <div
-      style={{ maxWidth: "480px" }}
-      className="mt-10 mb-20 p-4 md:p-7 mx-auto rounded bg-white shadow-lg"
-    >
-      <form onSubmit={submitHandler}>
-        <h2 className="mb-5 text-2xl font-semibold">Registrar cuenta</h2>
+  <div
+    style={{ maxWidth: "480px" }}
+    className="mt-10 mb-20 p-4 md:p-7 mx-auto rounded bg-white shadow-lg"
+  >
+    <form onSubmit={submitHandler} className="space-y-4">
+      <h2 className="text-2xl font-semibold text-center text-blue-600">
+        Registrar cuenta
+      </h2>
 
-        <div className="mb-4">
-          <label className="block mb-1"> Nombre Completo </label>
-          <input
-            className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
-            type="text"
-            placeholder="Ingresa tu nombre"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
+      <div>
+        <label className="block mb-1 font-medium">Nombre Completo</label>
+        <input
+          className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
+          type="text"
+          placeholder="Ingresa tu nombre"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
 
-        <div className="mb-4">
-          <label className="block mb-1"> Correo </label>
-          <input
-            className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
-            type="text"
-            placeholder="Ingresa tu correo"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
+      <div>
+        <label className="block mb-1 font-medium">Correo</label>
+        <input
+          className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
+          type="text"
+          placeholder="Ingresa tu correo"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
 
-        <div className="mb-4">
-          <label className="block mb-1"> Contraseña </label>
-          <input
-            className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
-            type="password"
-            placeholder="Ingresa tu contraseña"
-            minLength={6}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
+      <div>
+        <label className="block mb-1 font-medium">Contraseña</label>
+        <input
+          className="appearance-none border border-gray-200 bg-gray-100 rounded-md py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400 w-full"
+          type="password"
+          placeholder="Ingresa tu contraseña"
+          minLength={6}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
 
-        <button
-          type="submit"
-          className="my-2 px-4 py-2 text-center w-full inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700"
-          disabled={loading ? true : false}
-        >
-          {loading ? "Registrando..." : "Registrar"}
-        </button>
+      <button
+        type="submit"
+        className="my-2 px-4 py-2 text-center w-full inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700"
+        disabled={loading ? true : false}
+      >
+        {loading ? "Registrando..." : "Registrar"}
+      </button>
 
-        <hr className="mt-4" />
+      <hr className="mt-4" />
 
-        <p className="text-center mt-5">
+      <div className="text-center">
+        <p>
           Ya tienes una cuenta?{" "}
           <Link href="/login" className="text-blue-500">
             Ingresa
           </Link>
         </p>
-      </form>
-    </div>
-  );
+        <p className="font-medium">o</p>
+        <p className="mb-2">Registrate con Google</p>
+        <button
+          className="inline-flex items-center justify-center p-2 rounded-full bg-blue-600 text-white hover:bg-blue-700"
+          type="button"
+          onClick={() => signIn("google")}
+        >
+          <i className="fab fa-google"></i>
+        </button>
+      </div>
+    </form>
+  </div>
+);
 };
 
 export default Register;

31cac27396d4127108b4e810b1ab65836844fe43
luisillo619 <luiscarlosrangel619@gmail.com>
2023-07-22 18:17:01 -0600

    Commit final

--- src/components/review/Reviews.jsx
+++ src/components/review/Reviews.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import StarRatings from "react-star-ratings";
 
@@ -5,9 +6,9 @@ const Reviews = ({ reviews }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {reviews?.map((review) => (
-        <article className="block p-6 bg-white max-w-sm rounded-lg border border-gray-200 shadow-md mb-5">
+        <article key={review} className="block p-6 bg-white max-w-sm rounded-lg border border-gray-200 shadow-md mb-5">
           <div className="flex items-center mb-4 space-x-4">
-            <img
+            <Image
               className="w-10 h-10 rounded-full"
               src={
                 review?.user?.avatar
